044cd13f3a975da21bc686211975cd98e5a82548
JustineDevs <TraderGOfficial@gmail.com>
2026-03-20 18:51:59 +0800

    refactor: modify logic in apparel-commerce/apps/storefront/src/app/api/auth/[...nextauth]/route.ts

--- apparel-commerce/apps/storefront/src/app/api/auth/[...nextauth]/route.ts
+++ apparel-commerce/apps/storefront/src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  pages: {
+    signIn: "/sign-in",
+  },
 });
 
 export { handler as GET, handler as POST };
