94b750a3b202a5156ff5acbf275f14df9d160954
JustineDevs <TraderGOfficial@gmail.com>
2026-03-20 18:51:58 +0800

    apparel-commerce/apps/storefront/src/app/(public)/layout.tsx – Updated import { StorefrontNav } from "../../components/St to <StorefrontHeader

--- apparel-commerce/apps/storefront/src/app/(public)/layout.tsx
+++ apparel-commerce/apps/storefront/src/app/(public)/layout.tsx
@@ -1,5 +1,5 @@
-import { StorefrontNav } from "../../components/StorefrontNav";
 import { StorefrontFooter } from "../../components/StorefrontFooter";
+import { StorefrontHeader } from "../../components/StorefrontHeader";
 
 export default function PublicLayout({
   children,
@@ -8,8 +8,10 @@ export default function PublicLayout({
 }) {
   return (
     <>
-      <StorefrontNav />
-      <main className="pt-20">{children}</main>
+      <StorefrontHeader />
+      <div className="mx-auto w-full min-w-0 max-w-[100vw] pt-[5.75rem] xs:pt-[6rem] sm:pt-[6.5rem] md:pt-[7rem]">
+        {children}
+      </div>
       <StorefrontFooter />
     </>
   );
