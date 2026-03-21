cd34d7bbc3b961d00478528792c363fdb64951b0
JustineDevs <TraderGOfficial@gmail.com>
2026-03-21 23:09:29 +0800

    remove: delete apparel-commerce/apps/admin/next.config.js

--- apparel-commerce/apps/admin/next.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../.."),
-  transpilePackages: ['@apparel-commerce/ui', '@apparel-commerce/types', '@apparel-commerce/sdk'],
-};
-
-module.exports = nextConfig;
