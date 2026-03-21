ec2bcb2a1cb63d9864097d1aad659231c837d0d2
JustineDevs <TraderGOfficial@gmail.com>
2026-03-21 21:21:15 +0800

    refactor: modify logic in apparel-commerce/apps/admin/next.config.js

--- apparel-commerce/apps/admin/next.config.js
+++ apparel-commerce/apps/admin/next.config.js
@@ -1,5 +1,7 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../.."),
   transpilePackages: ['@apparel-commerce/ui', '@apparel-commerce/types', '@apparel-commerce/sdk'],
 };
 
