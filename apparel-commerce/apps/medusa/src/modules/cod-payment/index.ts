7d499d212f5d049365a924fb2ae1d6e3ed72cc7a
JustineDevs <TraderGOfficial@gmail.com>
2026-03-21 23:09:36 +0800

    remove: delete apparel-commerce/apps/medusa/src/modules/cod-payment/index.ts

--- apparel-commerce/apps/medusa/src/modules/cod-payment/index.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleProvider, Modules } from "@medusajs/framework/utils";
-import CodPaymentProviderService from "./service";
-
-export default ModuleProvider(Modules.PAYMENT, {
-  // AbstractPaymentProvider uses protected constructor; cast satisfies ModuleProvider's public-constructor expectation
-  services: [CodPaymentProviderService] as any,
-});
