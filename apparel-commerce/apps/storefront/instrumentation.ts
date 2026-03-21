6089feb4277c8a24f90f974753ac28176c088846
JustineDevs <TraderGOfficial@gmail.com>
2026-03-21 23:09:38 +0800

    remove: delete apparel-commerce/apps/storefront/instrumentation.ts

--- apparel-commerce/apps/storefront/instrumentation.ts
+++
@@ -1,9 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME !== "nodejs") {
-    return;
-  }
-  const { assertMedusaStorefrontEnvProduction } = await import(
-    "@apparel-commerce/sdk"
-  );
-  assertMedusaStorefrontEnvProduction();
-}
