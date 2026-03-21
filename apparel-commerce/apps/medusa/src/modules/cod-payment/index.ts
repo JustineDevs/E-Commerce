abdcef4c22249c0326e3dbba0608536978400ed5
JustineDevs <TraderGOfficial@gmail.com>
2026-03-21 21:21:15 +0800

    apparel-commerce/apps/medusa/src/modules/cod-payment/index.ts – Updated [CodPaymentProviderService], to [CodPaymentProviderService] as any,

--- apparel-commerce/apps/medusa/src/modules/cod-payment/index.ts
+++ apparel-commerce/apps/medusa/src/modules/cod-payment/index.ts
@@ -2,5 +2,6 @@ import { ModuleProvider, Modules } from "@medusajs/framework/utils";
 import CodPaymentProviderService from "./service";
 
 export default ModuleProvider(Modules.PAYMENT, {
-  services: [CodPaymentProviderService],
+  // AbstractPaymentProvider uses protected constructor; cast satisfies ModuleProvider's public-constructor expectation
+  services: [CodPaymentProviderService] as any,
 });
