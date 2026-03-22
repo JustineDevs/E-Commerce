61e6d2b4d3e00474cfb7a175fe91fa454390ee58
JustineDevs <TraderGOfficial@gmail.com>
2026-03-22 20:15:19 +0800

    refactor: modify logic in apps/admin/src/middleware.ts

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -1,6 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  pages: { signIn: "/api/auth/signin" },
   callbacks: {
     authorized: ({ token }) => {
       const r = token?.role as string | undefined;
