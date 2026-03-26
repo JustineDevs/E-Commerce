31b4df37f4375415834611565e2620559f412760
JustineDevs <TraderGOfficial@gmail.com>
2026-03-26 20:34:27 +0800

    refactor: modify logic in apps/admin/src/types/next-auth.d.ts

--- apps/admin/src/types/next-auth.d.ts
+++ apps/admin/src/types/next-auth.d.ts
@@ -1,19 +1,23 @@
+/* eslint-disable no-unused-vars */
 import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       role?: string;
+      permissions?: string[];
     } & DefaultSession["user"];
   }
 
   interface User {
     role?: string;
+    permissions?: string[];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     role?: string;
+    permissions?: string[];
   }
 }
