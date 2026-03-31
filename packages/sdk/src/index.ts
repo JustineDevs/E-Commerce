a321c65a5bb3caa499b5084b000c9af3f574a3c1
JustineDevs <TraderGOfficial@gmail.com>
2026-04-01 00:22:47 +0800

    refactor: modify logic in packages/sdk/src/index.ts

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -5,6 +5,7 @@ export {
   getMedusaPaymentProviderId,
   getMedusaSalesChannelId,
   getMedusaSecretApiKey,
+  withSalesChannelId,
 } from "./medusa-env";
 
 export {
@@ -20,3 +21,66 @@ export {
 export { assertAdminMedusaEnvProduction } from "./env/admin-medusa";
 export { DEFAULT_PUBLIC_SITE_ORIGIN } from "./public-site-url";
 export { PH_VAT_RATE, PH_VAT_PERCENT, computeDisplayVat } from "./ph-tax";
+
+export {
+  loadGoogleCredentials,
+  buildSharedJwtCallback,
+  buildSharedSessionCallback,
+  extractSessionEmail,
+  isSessionStaff,
+  normalizeEmail,
+  type SharedSessionUser,
+} from "./auth-shared";
+
+export {
+  type PrinterProfile,
+  type DrawerProfile,
+  type StoreHardwareConfig,
+  type HardwareHealthResult,
+  checkPrinterHealth,
+  isPeakHour,
+  runPeakHourHealthCheck,
+  DEFAULT_THERMAL_PRINTER,
+  DEFAULT_DRAWER,
+} from "./pos-hardware";
+
+export {
+  isCmsPubliclyVisible,
+  isMissingTableOrSchemaError,
+  loadCmsPagePublic,
+  loadCmsPagePreviewPublic,
+  loadCmsNavigationPublic,
+  loadCmsAnnouncementsPublic,
+  loadCmsAnnouncementPublic,
+  loadCmsCategoryContentPublic,
+  loadCmsBlogListPublic,
+  loadCmsBlogPostPublic,
+  loadCmsAbExperimentsActivePublic,
+  loadCmsSitemapEntries,
+  getCmsRedirectForPath,
+  CMS_FORM_KEYS,
+  buildCmsPreviewUrl,
+  buildCmsStoragePublicUrl,
+  normalizeCmsLocale,
+  pickCmsAbVariantId,
+  type CmsPageType,
+  type CmsPublishStatus,
+  type CmsNavLink,
+  type CmsNavFeatured,
+  type CmsFooterColumn,
+  type CmsSocialLink,
+  type CmsNavigationPayload,
+  type CmsBlock,
+  type CmsPageRow,
+  type CmsPageBlockPresetRow,
+  type CmsBlogPostRow,
+  type CmsRedirectRow,
+  type CmsAbExperimentRow,
+  type CmsAnnouncementRow,
+  type CmsCategoryContentRow,
+  type CmsMediaRow,
+  type CmsFormSettingsRow,
+  type CmsFormKey,
+  type BuildCmsPreviewUrlInput,
+  type CmsPreviewUrlKind,
+} from "./cms";
